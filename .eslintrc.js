0b8212459f74859403abaa0966b06ee2860d0e0e
Nathan Byrd <techy1@gmail.com>
2023-11-21 00:36:53 +0000

    Changed to use js format for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		jest: true,
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: 'xo',
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	rules: {
+		// Add your custom rules here
+	},
+};
